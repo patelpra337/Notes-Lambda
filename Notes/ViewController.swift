825ca9f07e162e0cf8bd1d4e5e0a7dcd2a9dd4d6
patelpra337 <patelpra337@gmail.com>
2019-04-29 20:42:49 -0500

    Add new Notes project

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by patelpra on 4/29/19.
+//  Copyright © 2019 Pravin Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
